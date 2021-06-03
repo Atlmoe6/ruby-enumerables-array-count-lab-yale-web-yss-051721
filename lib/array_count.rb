b69fd87a89e2b2878138b84b34a5c0bedea7f027
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-06-03 17:31:39 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -12,5 +12,5 @@ def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   
   array = [ "", "Hello", 4, [], "", "" ]
-  
+array.count do
 end
