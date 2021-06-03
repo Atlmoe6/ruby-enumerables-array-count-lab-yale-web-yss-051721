d63dd85097b9ee29f4391139f3d67e3341fe021e
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-06-03 17:21:49 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,17 @@
+require 'pry'
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  
+  
+    array.count do |String|
+      String.count
+      binding.pry
+ 
 end
-
+  
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  
+  array = [ "", "Hello", 4, [], "", "" ]
+  
 end
